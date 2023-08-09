7c54306ce9e190209cc2e5d23e83ac6a0deff86c
Ochieng24 <edwine.ochi24@gmail.com>
2023-08-09 09:23:40 +0000

    change to file 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
-    else:
-        print("{:02}".format(number), end=", ")
-
-
+print(", ".join("{:02}".format(i) for i in range(100)))
