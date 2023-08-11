31a52aaac29747ce99913e64a5b34959a3ae9ebd
Ochieng24 <edwine.ochi24@gmail.com>
2023-08-11 08:54:06 -0700

    loops and modules

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative_BACKUP_435.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    print("{} is positive".format(number))
+elif number == 0:
+    print("{} is zero".format(number))
+else:
+    print("{} is negative".format(number))
+<<<<<<< HEAD
+
+=======
+>>>>>>> cdec887f77613d69d8107433ebf5f7871f48c5dc
