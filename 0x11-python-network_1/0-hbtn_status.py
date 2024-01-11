49f337b76a34daee8e66b7d423d17ea4c592f081
Ochieng24 <edwine.ochi24@gmail.com>
2024-01-11 05:13:46 -0800

    changes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-#  script that fetches https://alx-intranet.hbtn.io/status
-from urllib import request, error
-# store the url in a variable
-url = "https://alx-intranet.hbtn.io/status"
-# open the url and read the response
-try:
-    with request.urlopen(url) as response:
+"""Fetches data from given URL"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+            as response:
         html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
-except error.HTTPError as error:
-    print("Error code: {}".format(error.code))
+
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
